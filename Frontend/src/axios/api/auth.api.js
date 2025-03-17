abd2a12b5555c7513d859a0400bc247d79cefeec
VAIBHAV DEEPAK SHINDE <123886126+vaibhavshinde8@users.noreply.github.com>
2025-03-17 14:23:22 +0530

    changes

--- Frontend/src/axios/api/auth.api.js
+++ Frontend/src/axios/api/auth.api.js
@@ -1,39 +1,27 @@
 
 import axiosInstance from "../axiosConfig";
-async function  login(data) {
+async function login(email,password,userType) {//"User"
     try {
-        if(data.googleId){
-            const response = await axiosInstance.post("/auth/login", { googleId: data.googleId ,email:data.email,userType:data.userType});
-            return response.data;
-        }
-        else{
-            const response = await axiosInstance.post("/auth/login",{email:data.email,password:data.password,userType:data.userType});
-            return response.data;
-        }
-    } catch (error) {
-        return error.response.data;
-    }
-}
-async function  register(data) {
-    try {
-        if(data.googleId){
-            const response = await axiosInstance.post("/auth/register", { googleId: data.googleId ,email:data.email,userType:data.userType});
-            return response.data;
-        }
-        else{
-            const response = await axiosInstance.post("/auth/register",{email:data.email,password:data.password,userType:data.userType});
-            return response.data;
-        }
-    }
-catch(error){
-    return error.response.data;}
-}
-async function  resetPassword(data) {
-    try {
-        const response = await axiosInstance.post("/auth/reset-password", { email: data.email ,newPassword:data.newPassword,isConformed:data.isConformed});
+        const response = await axiosInstance.post("/auth/login", { email, password, userType });
+        console.log(response.data);
         return response.data;
+
     } catch (error) {
+        console.log(error.response.data);
         return error.response.data;
     }
 }
-export default {login,register,resetPassword};
+async function register(email,password,userType,conformPassword) {//"User"
+    try {
+        const response = await axiosInstance.post("/auth/register", { email, password, userType ,conformPassword});
+        console.log(response.data);
+        return response.data;
+
+    } catch (error) {
+        console.log(error.response.data);
+        return error.response.data;
+    }
+}
+
+
+export  { login };
