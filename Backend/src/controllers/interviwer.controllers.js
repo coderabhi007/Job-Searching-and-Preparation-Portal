c4113d6cc9d5daba87dc1d53e344050b1c5db60f
Abhishek Sutar <sutarabhishek711@gmail.com>
2025-04-16 08:24:10 +0530

    all done

--- Backend/src/controllers/interviwer.controllers.js
+++ Backend/src/controllers/interviwer.controllers.js
@@ -2,6 +2,7 @@ import { ApiResponse } from "../util/ApiResponse.js";
 import { ApiError } from "../util/ApiError.js";
 import Interviewer from "../models/interviwer.model.js";
 import  {auth as Auth}  from "../models/auth.model.js";
+import { uploadOnCloudinary } from "../util/Cloudnary.js";
 const createProfile = async (req, res) => {
   try {
     const authId = req.user._id;
@@ -32,7 +33,7 @@ const createProfile = async (req, res) => {
     return res.status(201).json(new ApiResponse(201, profile, "Profile created successfully"));
 
   } catch (error) {
-    //console.log(error);
+    console.log(error);
     return res.status(500).json(new ApiError(500, error.message));
   }
 };
@@ -109,19 +110,25 @@ const updateProfile = async (req, res) => {
   
   async function addProfilePic(req,res){
       try {
+        //console.log("Enterd")
           const authId=req.user._id
           //check if user already exists
+         // console.log("user finding started")
           const user=await Interviewer.findOne({authId:authId})
+        //  console.log("user finding ended")
           if(!user){
               return res.status(400).json(new ApiError(400, "User not found."));
           }
-          const file=req.files.profilePic[0];
+          const file=req.files?.profilePic[0];
+         // console.log("file+="+file);
           const result=await uploadOnCloudinary(file.path,"profilePic")
+         console.log("result"+result)
           //update profilePic in database
           user.profileImage=result.secure_url;
-          const savedUser=await Interviewer.save();
+          const savedUser=await user.save();
           return res.status(200).json(new ApiResponse(200,savedUser,"Profile picture updated successfully"));
       } catch (error) {
+        console.log(error)
           return res.status(500).json(new ApiError(500, "Internal Server Error", error.message));
       }
   }
